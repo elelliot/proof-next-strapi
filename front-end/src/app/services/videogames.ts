d4411ef4e95f6af30f8974a4b9d6c90a79b8c92f
Elliot Miranda <elliot_150894@hotmail.com>
2023-10-05 16:31:42 -0700

    Creates Videogame site with NextJS and Strapi

---
+++ front-end/src/app/services/videogames.ts
@@ -0,0 +1,21 @@
+import { API_URL, STRAPI_URL } from "@/app/config";
+
+export async function getGames({ page = 1 }) {
+  const res = await fetch(
+    `${API_URL}/videogames?populate[platforms][fields][0]=name&populate[cover][fields][0]=url&pagination[page]=${page}&pagination[pageSize]=1`,
+    {
+      cache: "no-store",
+    }
+  );
+  if (!res.ok) {
+    throw new Error("Something went wrong");
+  }
+  const { data, meta } = await res.json();
+  const { pagination } = meta;
+  return { data, pagination };
+}
+
+export function getCoverImage({ attributes }: any) {
+  const { url } = attributes.cover.data.attributes;
+  return `${STRAPI_URL}${url}`;
+}
